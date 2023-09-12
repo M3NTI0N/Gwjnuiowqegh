4134b5eea1723d0d24c4a338d297f08a712321b4
Mark Walther <xdaserver@gmail.com>
2023-09-12 18:33:02 +0200

    Update ingame-time.js

--- js/ingame-time.js
+++ js/ingame-time.js
@@ -5,7 +5,7 @@ export function updateInGameTime(inGameTimeDecimal) {
 	const floorHour = Math.floor(inGameTimeDecimal);
 	const ingameHour = Util.getTwelveHour(floorHour);
 	const ingameMinute = Math.floor((inGameTimeDecimal % 1) * 60);
-    const hourDeg = (360 / 24) * (ingameHour + ingameMinute / 60);
+    const hourDeg = (360 / 12) * (ingameHour + ingameMinute / 60);
 
 	window.ingameTimeDisplay = `${ingameHour}:${ingameMinute.toString().padStart(2, '0')} ${Util.getMeridiemText(floorHour)}`;
     document.getElementById('hour-hand').style.transform = `translateX(-50%) rotate(${hourDeg}deg)`;
