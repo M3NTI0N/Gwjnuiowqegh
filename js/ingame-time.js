631cff5ddd11b94732edc5f0876565e65782615e
Mark Walther <xdaserver@gmail.com>
2023-09-12 21:15:35 +0200

    Update ingame-time.js

--- js/ingame-time.js
+++ js/ingame-time.js
@@ -8,7 +8,7 @@ export function updateInGameTime(inGameTimeDecimal) {
     const hours = floorHour % 24;
     const hourDegrees = (360 / 24) * hours + (360 / 24) * (ingameMinute / 60);
     const hourElement = document.getElementById('hour');
-    if (floorHour > 6 && floorHour < 18) {
+    if (floorHour >= 6 && floorHour <= 17) {
         document.getElementById('clock-container').style.backgroundImage = "url('/img/innersun.png')";
 	} else {
 		document.getElementById('clock-container').style.backgroundImage = "url('/img/innermoon.png')";
