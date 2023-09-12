7da4f0c83d894e886db247b0b5cab730a5b48915
Mark Walther <xdaserver@gmail.com>
2023-09-12 23:55:08 +0200

    Update ingame-time.js

--- js/ingame-time.js
+++ js/ingame-time.js
@@ -13,7 +13,7 @@ export function updateInGameTime(inGameTimeDecimal) {
     const prefixedmorningGradient = 'linear-gradient(to bottom, #fddb92, #d1fdff)';
     const prefixedafternoonGradient = 'linear-gradient(to bottom, #44bfc3, #9eb8f0)';
     const prefixedeveningGradient = 'linear-gradient(to bottom, #56374B 0%, #875A6F 40%, #AB7682 100%)';
-    const prefixednightGradient = 'linear-gradient(190deg, #1A1840 0%, #3B2146 40%, #56374B 100%)';
+    const prefixednightGradient = 'linear-gradient(170deg, #1A1840 0%, #3B2146 40%, #56374B 100%)';
     
     // Change Background at times.
     if (floorHour >= 5 && floorHour < 12) {
