7cb045ad31726e82299897373cfb45ae2cf3c591
Mark Walther <xdaserver@gmail.com>
2023-09-12 21:39:46 +0200

    tewgrh

--- js/analogclock.js
+++
@@ -1,7 +0,0 @@
-export function onToggleShowStatus() {
-	const showAllBooleanValue = document.getElementById("toggle-headerstatus").checked;
-
-	window.localStorage.setItem('show-all', showAllBooleanValue);
-
-	document.getElementById("header-status").setAttribute('data-show-all', showAllBooleanValue);
-}
