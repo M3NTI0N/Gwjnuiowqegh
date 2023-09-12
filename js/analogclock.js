72ca15bae31fc8f56658a658926833b792fc935b
Mark Walther <xdaserver@gmail.com>
2023-09-12 12:32:24 +0200

    Update analogclock.js

--- js/analogclock.js
+++ js/analogclock.js
@@ -1,20 +1,21 @@
-function updateClockHands() {
-    const now = new Date();
-    const hours = now.getHours() % 12; // Convert to 12-hour format
-    const minutes = now.getMinutes();
-    const seconds = now.getSeconds();
-    
+import * as Util from './util.js';
+import * as TabManager from './tab-manager.js';
+
+export function updateClockHands(inGameTimeDecimal) {
+	const now = Math.floor(inGameTimeDecimal);
+	const hours = Util.getTwelveHour(now) % 12;
+	const minutes = Math.floor((inGameTimeDecimal % 1) * 60);
+
     // Calculate rotation angles for the clock hands
     const hourDeg = (360 / 12) * (hours + minutes / 60);
     const minuteDeg = (360 / 60) * minutes;
     const secondDeg = (360 / 60) * seconds;
-
-    // Update the CSS transforms for the clock hands
     document.getElementById('hour-hand').style.transform = `translateX(-50%) rotate(${hourDeg}deg)`;
     document.getElementById('minute-hand').style.transform = `translateX(-50%) rotate(${minuteDeg}deg)`;
-    // Uncomment the following line if you want to add a second hand
-    // document.getElementById('second-hand').style.transform = `translateX(-50%) rotate(${secondDeg}deg)`;
+
+	TabManager.updateTitle();
 }
+
 
 // Call the updateClockHands function every second to keep the clock updated
 setInterval(updateClockHands, 1000);
