f246a25271306da07c3c133a3105be33f265bc0d
Mark Walther <xdaserver@gmail.com>
2023-09-12 17:36:52 +0200

    Changes back the say.js to default

--- js/say.js
+++ js/say.js
@@ -1,42 +1,48 @@
 export function doSpeak(theText) {
+	
+  // let utterance = new SpeechSynthesisUtterance('cat');
+
+  // utterance.text = theText;
+	// window.speechSynthesis.cancel();
+	// window.speechSynthesis.speak(utterance);
+
+
 	let speech = new SpeechSynthesisUtterance(theText);
 
 	speech.text = theText;
-  
+	
 	const voices = window.speechSynthesis.getVoices();
-  
-	const { voice } = _getFemaleVoice(voices);
-  
-	speech.voice = voice;
-  
+	
+	const { isDefault, voice } = _getVoice(voices);
+
+	speech.default = isDefault;
+	
+  speech.voice = voice;
+
 	speech.lang = "en-GB";
-  
-	window.speechSynthesis.speak(speech);
+	
+  window.speechSynthesis.speak(speech);
 }
 
 const _getVoice = (voices) => {
 	let foundVoice = voices.find(function (voice) {
-		return voice.name === "Google UK English Female";
-	  });
+    return voice.name === "Google UK English Female";
+	});
 	
-	  if (foundVoice) {
-		return { voice: foundVoice };
-	  }
-	
-	  foundVoice = voices.find(function (voice) {
+	if (foundVoice) {
+		return { voice: foundVoice, isDefault: false };
+	}
+
+	foundVoice = voices.find(function (voice) {
 		return voice.name === "Microsoft Linda - English (Canada)";
-	  });
+	});
 	
-	  if (foundVoice) {
-		return { voice: foundVoice };
-	  }
-	
-	  // If no specific female voice is found, fallback to the first available female voice
-	  foundVoice = voices.find(function (voice) {
-		return voice.gender === "female";
-	  });
-	
-	  return {
-		voice: foundVoice || voices[0], // Fallback to the first available voice if no female voice is found
-	  };
+	if (foundVoice) {
+		return { voice: foundVoice, isDefault: false };
+	}
+
+	return {
+		voice: voices[1],
+		isDefault: true
+	};
 }
