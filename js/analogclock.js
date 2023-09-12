6fa7e6751e14140b81907df8dd9308ae7efa308e
Mark Walther <xdaserver@gmail.com>
2023-09-12 17:35:05 +0200

    changes analog clock

--- js/analogclock.js
+++ js/analogclock.js
@@ -1,44 +1,26 @@
 import * as Util from './util.js';
 import * as TabManager from './tab-manager.js';
 
-export function updateClockHands(inGameTimeDecimal) {
-	const now = Math.floor(inGameTimeDecimal);
-	const hours = Util.getTwelveHour(now) % 12;
-	const minutes = Math.floor((inGameTimeDecimal % 1) * 60);
+export function updateInGameTime(inGameTimeDecimal) {
+    const floorHour = Math.floor(inGameTimeDecimal);
+	const ingameHour = Util.getTwelveHour(floorHour);
+	const ingameMinute = Math.floor((inGameTimeDecimal % 1) * 60);
 
-    // Calculate rotation angles for the clock hands
-    const hourDeg = (360 / 12) * (hours + minutes / 60);
-    const minuteDeg = (360 / 60) * minutes;
-
-    document.getElementById('hour-hand').style.transform = `translateX(-50%) rotate(${hourDeg}deg)`;
-    document.getElementById('minute-hand').style.transform = `translateX(-50%) rotate(${minuteDeg}deg)`;
-
+	window.ingameTimeDisplay = `${ingameHour}:${ingameMinute.toString().padStart(2, '0')} ${Util.getMeridiemText(floorHour)}`;
+	document.getElementById('ingame-time').innerText = ingameTimeDisplay;
 	TabManager.updateTitle();
 }
+// Function to update the clock hands based on the in-game time
+function updateClockHands(ingameHour, ingameMinute) {
+    // Calculate rotation angles for the clock hands
+    const hourDeg = (360 / 12) * (ingameHour + ingameMinute / 60);
+    const minuteDeg = (360 / 60) * ingameMinute;
+    // Update the CSS transforms for the clock hands
+    document.getElementById('hour-hand').style.transform = `translateX(-50%) rotate(${hourDeg}deg)`;
+    document.getElementById('minute-hand').style.transform = `translateX(-50%) rotate(${minuteDeg}deg)`;
+}
 
-
-// Call the updateClockHands function every second to keep the clock updated
-setInterval(updateClockHands, 1000);
-
-// Initial call to set the clock hands at the current time
-updateClockHands();
-
-        // Your JavaScript code here
-
-        // Function to update the clock hands based on the in-game time
-        function updateClockHands(ingameHour, ingameMinute) {
-            // Calculate rotation angles for the clock hands
-            const hourDeg = (360 / 12) * (ingameHour + ingameMinute / 60);
-            const minuteDeg = (360 / 60) * ingameMinute;
-
-            // Update the CSS transforms for the clock hands
-            document.getElementById('hour-hand').style.transform = `translateX(-50%) rotate(${hourDeg}deg)`;
-            document.getElementById('minute-hand').style.transform = `translateX(-50%) rotate(${minuteDeg}deg)`;
-            // Uncomment the following line if you want to add a second hand
-            // document.getElementById('second-hand').style.transform = `translateX(-50%) rotate(${secondDeg}deg)`;
-        }
-
-        // Call the updateClockHands function with in-game time values
-        const ingameHour = 12; // Replace with your in-game hour
-        const ingameMinute = 30; // Replace with your in-game minute
-        updateClockHands(ingameHour, ingameMinute);
+// Call the updateClockHands function with in-game time values
+const ingameHours = ingameHour; // Replace with your in-game hour
+const ingameMinutes = ingameMinute; // Replace with your in-game minute
+updateClockHands(ingameHours, ingameMinutes);
