af9383d121033e7c47624e7a1ec761871488abcc
Mark Walther <xdaserver@gmail.com>
2023-09-12 18:19:34 +0200

    analogclock.js changed

--- js/analogclock.js
+++ js/analogclock.js
@@ -5,22 +5,13 @@ export function updateInGameTime(inGameTimeDecimal) {
     const floorHour = Math.floor(inGameTimeDecimal);
 	const ingameHour = Util.getTwelveHour(floorHour);
 	const ingameMinute = Math.floor((inGameTimeDecimal % 1) * 60);
+    const hourDeg = (360 / 12) * (ingameHour + ingameMinute / 60);
+    const minuteDeg = (360 / 60) * ingameMinute;
+
 
 	window.ingameTimeDisplay = `${ingameHour}:${ingameMinute.toString().padStart(2, '0')} ${Util.getMeridiemText(floorHour)}`;
+    document.getElementById('hour-hand').style.transform = `translateX(-50%) rotate(${hourDeg}deg)`;
+    document.getElementById('minute-hand').style.transform = `translateX(-50%) rotate(${minuteDeg}deg)`;
 	document.getElementById('ingame-time').innerText = ingameTimeDisplay;
 	TabManager.updateTitle();
 }
-// Function to update the clock hands based on the in-game time
-function updateClockHands(ingameHour, ingameMinute) {
-    // Calculate rotation angles for the clock hands
-    const hourDeg = (360 / 12) * (ingameHour + ingameMinute / 60);
-    const minuteDeg = (360 / 60) * ingameMinute;
-    // Update the CSS transforms for the clock hands
-    document.getElementById('hour-hand').style.transform = `translateX(-50%) rotate(${hourDeg}deg)`;
-    document.getElementById('minute-hand').style.transform = `translateX(-50%) rotate(${minuteDeg}deg)`;
-}
-
-// Call the updateClockHands function with in-game time values
-const ingameHours = ingameHour; // Replace with your in-game hour
-const ingameMinutes = ingameMinute; // Replace with your in-game minute
-updateClockHands(ingameHours, ingameMinutes);
