6cb8a05a9212d4368b8ecfe637af8066aefdfdc5
Mark Walther <xdaserver@gmail.com>
2023-09-12 18:30:58 +0200

    analog clock changes

--- js/ingame-time.js
+++ js/ingame-time.js
@@ -8,10 +8,8 @@ export function updateInGameTime(inGameTimeDecimal) {
     const hourDeg = (360 / 24) * (ingameHour + ingameMinute / 60);
     //const minuteDeg = (360 / 60) * ingameMinute;
 
-
 	window.ingameTimeDisplay = `${ingameHour}:${ingameMinute.toString().padStart(2, '0')} ${Util.getMeridiemText(floorHour)}`;
     document.getElementById('hour-hand').style.transform = `translateX(-50%) rotate(${hourDeg}deg)`;
-    document.getElementById('minute-hand').style.transform = `translateX(-50%) rotate(${minuteDeg}deg)`;
 	document.getElementById('ingame-time').innerText = ingameTimeDisplay;
 	TabManager.updateTitle();
 }
