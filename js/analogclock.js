b506d7a1f8a0df434b36f791160eee1d6b844747
Mark Walther <xdaserver@gmail.com>
2023-09-12 17:30:10 +0200

    Changing analog clock

--- js/analogclock.js
+++ js/analogclock.js
@@ -22,3 +22,23 @@ setInterval(updateClockHands, 1000);
 
 // Initial call to set the clock hands at the current time
 updateClockHands();
+
+        // Your JavaScript code here
+
+        // Function to update the clock hands based on the in-game time
+        function updateClockHands(ingameHour, ingameMinute) {
+            // Calculate rotation angles for the clock hands
+            const hourDeg = (360 / 12) * (ingameHour + ingameMinute / 60);
+            const minuteDeg = (360 / 60) * ingameMinute;
+
+            // Update the CSS transforms for the clock hands
+            document.getElementById('hour-hand').style.transform = `translateX(-50%) rotate(${hourDeg}deg)`;
+            document.getElementById('minute-hand').style.transform = `translateX(-50%) rotate(${minuteDeg}deg)`;
+            // Uncomment the following line if you want to add a second hand
+            // document.getElementById('second-hand').style.transform = `translateX(-50%) rotate(${secondDeg}deg)`;
+        }
+
+        // Call the updateClockHands function with in-game time values
+        const ingameHour = 12; // Replace with your in-game hour
+        const ingameMinute = 30; // Replace with your in-game minute
+        updateClockHands(ingameHour, ingameMinute);
