b7d76f74f49b5a19f21888b51d7ac5538028fbdd
Mark Walther <xdaserver@gmail.com>
2023-09-12 21:26:31 +0200

    remove fold

--- js/analogclock.js
+++
@@ -1,8 +0,0 @@
-function toggleFoldable() {
-	var content = document.getElementById('header-status');
-	if (content.style.display === 'none' || content.style.display === '') {
-		content.style.display = 'block';
-	} else {
-		content.style.display = 'none';
-	}
-}
