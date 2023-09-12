c182c2f30323a896053a48d6a75188cda24a6666
Mark Walther <xdaserver@gmail.com>
2023-09-12 19:26:17 +0200

    changed analog clock

--- js/analogclock.js
+++ js/analogclock.js
@@ -4,6 +4,7 @@ import * as TabManager from './tab-manager.js';
 export function updateInGameTime(inGameTimeDecimal) {
     const floorHour = Math.floor(inGameTimeDecimal);
 	const ingameHour = Util.getTwelveHour(floorHour);
+	const ingameMinute = Math.floor((inGameTimeDecimal % 1) * 60);
 
 	window.ingameTimeDisplay = `${ingameHour}:${ingameMinute.toString().padStart(2, '0')} ${Util.getMeridiemText(floorHour)}`;
 	document.getElementById('ingame-time').innerText = ingameTimeDisplay;
--- js/ingame-time.js
+++ js/ingame-time.js
@@ -5,20 +5,15 @@ export function updateInGameTime(inGameTimeDecimal) {
 	const floorHour = Math.floor(inGameTimeDecimal);
 	const ingameHour = Util.getTwelveHour(floorHour);
     const ingameMinute = Math.floor((inGameTimeDecimal % 1) * 60);
-    let hourDeg;
-    if (Util.getMeridiemText(floorHour) === "PM") {
-        // Calculate hourDeg for PM
-        hourDeg = (360 / 24) * (ingameHour + 12 + ingameMinute / 60);
-    } else {
-        // Calculate hourDeg for AM
-        hourDeg = (360 / 24) * (ingameHour + ingameMinute / 60);
-    }
-    //const hourDeg = (360 / 24) * (ingameHour + ingameMinute / 60);
+    const hours = floorHour % 24;
+    const hourDegrees = (360 / 12) * hours + (360 / 12) * (minutes / 60);
+    const hourElement = document.getElementById('hour');
 
     
 
 	window.ingameTimeDisplay = `${ingameHour}:${ingameMinute.toString().padStart(2, '0')} ${Util.getMeridiemText(floorHour)}`;
-    document.getElementById('hour-hand').style.transform = `translateX(-50%) rotate(${hourDeg}deg)`;
+    hourElement.style.transform = `rotate(${hourDegrees}deg)`;
+    
     document.getElementById('ingame-time').innerText = ingameTimeDisplay;
 	TabManager.updateTitle();
 }
