7b347519864446dd7d3e56b4c25421cdc036f4a2
Mark Walther <xdaserver@gmail.com>
2023-09-12 22:46:16 +0200

    Update ingame-time.js

--- js/ingame-time.js
+++ js/ingame-time.js
@@ -9,51 +9,51 @@ export function updateInGameTime(inGameTimeDecimal) {
     const hours = floorHour % 24;
     const hourDegrees = (360 / 24) * hours + (360 / 24) * (ingameMinute / 60);
     const hourElement = document.getElementById('hour');
+    document.addEventListener('DOMContentLoaded', function() {
+        // Gradients
+        const morninggradient = 'linear-gradient(to bottom, #94c5f8 1%,#a6e6ff 70%,#b1b5ea 100%)';
+        const prefixedmorningGradient = [
+            '-webkit-' + morninggradient,
+            '-moz-' + morninggradient,
+            '-o-' + morninggradient,
+            morninggradient
+        ].join(';');
 
-    // Gradients
-    const morninggradient = 'linear-gradient(to bottom, #94c5f8 1%,#a6e6ff 70%,#b1b5ea 100%)';
-    const prefixedmorningGradient = [
-        '-webkit-' + morninggradient,
-        '-moz-' + morninggradient,
-        '-o-' + morninggradient,
-        morninggradient
-    ].join(';');
+        const afternoongradient = 'linear-gradient(to bottom, #2d91c2 0%,#1e528e 100%)';
+        const prefixedafternoonGradient = [
+            '-webkit-' + afternoongradient,
+            '-moz-' + afternoongradient,
+            '-o-' + afternoongradient,
+            afternoongradient
+        ].join(';');
 
-    const afternoongradient = 'linear-gradient(to bottom, #2d91c2 0%,#1e528e 100%)';
-    const prefixedafternoonGradient = [
-        '-webkit-' + afternoongradient,
-        '-moz-' + afternoongradient,
-        '-o-' + afternoongradient,
-        afternoongradient
-    ].join(';');
+        const eveninggradient = 'linear-gradient(to bottom, #154277 0%,#576e71 30%,#e1c45e 70%,#b26339 100%)';
+        const prefixedeveningGradient = [
+            '-webkit-' + eveninggradient,
+            '-moz-' + eveninggradient,
+            '-o-' + eveninggradient,
+            eveninggradient
+        ].join(';');
 
-    const eveninggradient = 'linear-gradient(to bottom, #154277 0%,#576e71 30%,#e1c45e 70%,#b26339 100%)';
-    const prefixedeveningGradient = [
-        '-webkit-' + eveninggradient,
-        '-moz-' + eveninggradient,
-        '-o-' + eveninggradient,
-        eveninggradient
-    ].join(';');
+        const nightgradient = 'linear-gradient(to bottom, #40405c 0%,#6f71aa 80%,#8a76ab 100%)';
+        const prefixednightGradient = [
+            '-webkit-' + nightgradient,
+            '-moz-' + nightgradient,
+            '-o-' + nightgradient,
+            nightgradient
+        ].join(';');
 
-    const nightgradient = 'linear-gradient(to bottom, #40405c 0%,#6f71aa 80%,#8a76ab 100%)';
-    const prefixednightGradient = [
-        '-webkit-' + nightgradient,
-        '-moz-' + nightgradient,
-        '-o-' + nightgradient,
-        nightgradient
-    ].join(';');
-
-    // Change Background at times.
-    if (floorHour > 5 && floorHour < 10) {
-        document.body.style.background = prefixedmorningGradient;
-    } else if (floorHour > 10 && floorHour < 15) {
-        document.body.style.background = prefixedafternoonGradient;
-    } else if (floorHour > 15 && floorHour < 19) {
-        document.body.style.background = prefixedeveningGradient;
-    } else {
-        document.body.style.background = prefixednightGradient;
+        // Change Background at times.
+        if (floorHour > 5 && floorHour < 10) {
+            document.body.style.background = prefixedmorningGradient;
+        } else if (floorHour > 10 && floorHour < 15) {
+            document.body.style.background = prefixedafternoonGradient;
+        } else if (floorHour > 15 && floorHour < 19) {
+            document.body.style.background = prefixedeveningGradient;
+        } else {
+            document.body.style.background = prefixednightGradient;
+        }
     }
-
     // Time counters/clock
     if (floorHour > 5 && floorHour < 18) {
         document.getElementById('clock-container').style.backgroundImage = "url('/img/innersun.png')";
