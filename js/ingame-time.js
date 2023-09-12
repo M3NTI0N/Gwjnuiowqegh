5f63e9194618fb5f76e45a5e25ceb46ff4d5a9fa
Mark Walther <xdaserver@gmail.com>
2023-09-12 10:45:01 +0200

    without analog clock

--- js/ingame-time.js
+++ js/ingame-time.js
@@ -6,19 +6,6 @@ export function updateInGameTime(inGameTimeDecimal) {
 	const ingameHour = Util.getTwelveHour(floorHour);
 	const ingameMinute = Math.floor((inGameTimeDecimal % 1) * 60);
 
-    const hourElement = document.getElementById('hour');
-    const minuteElement = document.getElementById('minute');
-
-    const now = new Date();
-    const hours = ingameHour % 12;
-    const minutes = ingameMinute.toString().padStart(2, '0');
-
-    const hourDegrees = (360 / 12) * hours + (360 / 12) * (minutes / 60);
-    const minuteDegrees = (360 / 60) * minutes + (360 / 60) * (seconds / 60);
-
-    hourElement.style.transform = `rotate(${hourDegrees}deg)`;
-    minuteElement.style.transform = `rotate(${minuteDegrees}deg)`;
-
 	window.ingameTimeDisplay = `${ingameHour}:${ingameMinute.toString().padStart(2, '0')} ${Util.getMeridiemText(floorHour)}`;
 	document.getElementById('ingame-time').innerText = ingameTimeDisplay;
 	TabManager.updateTitle();
